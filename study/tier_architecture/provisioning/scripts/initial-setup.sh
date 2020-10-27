4970c72d6eced4a0554f615e300961a38bf457de
LeeKeunJe <nigasa12@gmail.com>
2020-10-27 22:37:32 +0900

    [study] modify initial-setup

--- study/tier_architecture/provisioning/scripts/initial-setup.sh
+++ study/tier_architecture/provisioning/scripts/initial-setup.sh
@@ -3,5 +3,4 @@
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo systemctl restart sshd
 
-sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
-sudo reboot
+sudo setenforce 0
